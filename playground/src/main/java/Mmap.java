d67b20748a2432026fc7e70203c70b47f42180fa
yche <yche@cse.ust.hk>
2017-05-30 13:06:09 -0400

    update

--- playground/src/main/java/Mmap.java
+++ playground/src/main/java/Mmap.java
@@ -1,4 +1,7 @@
+import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 
@@ -23,4 +26,37 @@ public class Mmap {
         }
     }
 
+    static void unmap(MappedByteBuffer mbb) {
+        try {
+            Method cleaner = mbb.getClass().getMethod("cleaner");
+            cleaner.setAccessible(true);
+            Method clean = Class.forName("sun.misc.Cleaner").getMethod("clean");
+            clean.invoke(cleaner.invoke(mbb));
+            System.out.println("unmap successful");
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        RandomAccessFile memoryMappedFile = new RandomAccessFile("/tmp/bigFile", "rw");
+        FileChannel fileChannel = memoryMappedFile.getChannel();
+        MappedByteBuffer mBuf = fileChannel.map(FileChannel.MapMode.READ_WRITE, 0, memoryMappedFile.length());
+        mBuf.load();
+
+        System.out.println(mBuf.limit());
+        for (int i = 0; i < mBuf.limit(); i++) {
+            System.out.println((char) mBuf.get());
+        }
+        memoryMappedFile.close();
+//      unmap(fileChannel, mBuf);
+        unmap(mBuf);
+    }
 }
