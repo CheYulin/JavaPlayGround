d7a82f2dfd61b39c4f872044bfcd4d50572ae351
CHE Yulin <yche@cse.ust.hk>
2017-06-03 09:31:47 +0800

    add compression study

---
+++ playground/src/main/java/Compression.java
@@ -0,0 +1,53 @@
+import java.io.ByteArrayOutputStream;
+import java.util.Arrays;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
+
+/**
+ * Created by yche on 6/3/17.
+ */
+public class Compression {
+    public static void main(String[] argv) throws Exception {
+        byte[] input = "www.everycoding.com".getBytes();
+
+        Deflater compressor = new Deflater();
+        compressor.setLevel(Deflater.BEST_COMPRESSION);
+        compressor.setInput(input);
+        compressor.finish();
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream(input.length);
+
+        byte[] buf = new byte[1024];
+        while (!compressor.finished()) {
+            int count = compressor.deflate(buf);
+            bos.write(buf, 0, count);
+        }
+        bos.close();
+        byte[] compressedData = bos.toByteArray();
+        System.out.println("压缩后的byte结果：" + Arrays.toString(compressedData));
+        // 对压缩数据进行解压
+        decompressor(compressedData);
+    }
+
+    /**
+     * 对压缩数据进行解压
+     *
+     * @param compressedData
+     * @throws Exception
+     */
+    public static void decompressor(byte[] compressedData) throws Exception {
+        Inflater decompressor = new Inflater();
+        decompressor.setInput(compressedData);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream(compressedData.length);
+        byte[] buf = new byte[1024];
+        while (!decompressor.finished()) {
+            int count = decompressor.inflate(buf);
+            bos.write(buf, 0, count);
+
+        }
+        bos.close();
+        byte[] decompressedData = bos.toByteArray();
+        System.out.println("解压后的byte结果：" + Arrays.toString(decompressedData));
+        System.out.println("解压出字符串：" + bos.toString());
+    }
+}
